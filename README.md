2075211ef23f0516d61748aa8712c8366e5a1bdb
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-12-03 00:45:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pierre-papier-ciseaux
+
+Plongez dans l'historique pour comprendre l'évolution de ce petit projet.
---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Coup.Pierre.gagneContre(Coup.Ciseaux));
+        System.out.println(Coup.Pierre);
+    }
+}

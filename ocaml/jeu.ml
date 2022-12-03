bf71e50f409c1be6152782f5fa426887e6f0df2a
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-12-03 17:30:19 +0100

    Élimination du foncteur

--- ocaml/jeu.ml
+++ ocaml/jeu.ml
@@ -1,32 +1,28 @@
-module type R = sig
-  type coup
-  val coups : coup list
-  val to_string : coup -> string
-  val of_string : string -> coup
-  val (>>) : coup -> coup -> bool
-end
+  open Coup
 
-module type S = sig
-  val main : unit -> unit
-end
-
-module Make (Regles : R) : S = struct
-  open Regles
-
-  let affiche_tableau () =
-    List.iter (fun x -> Printf.printf "\t%s" (to_string x)) coups;
+  let affiche_tableau l =
+    List.iter (fun x -> Printf.printf "\t%s" (to_string x)) l;
     Printf.printf "\n";
-    ListLabels.iter coups ~f:(fun x ->
+    ListLabels.iter l ~f:(fun x ->
       Printf.printf "%s\t" (to_string x);
-      ListLabels.iter coups ~f:(fun y ->
+      ListLabels.iter l ~f:(fun y ->
         Printf.printf "%s\t" (if x = y then "" else if x >> y then "<" else "|")
       );
       Printf.printf "\n"
     )
 
-  let main () =
+  let () =
+    let coups =
+      Printf.printf "Voulez-vous jouer avec le puits ? %!";
+      match read_line () with
+      | "oui" | "yes" -> Coup.coups
+      | x ->
+        if x <> "non" && x <> "no" then
+          Printf.printf "On va dire que non...\n";
+        List.map (fun x -> Std x) Standard.coups
+    in
     Printf.printf "Petit rappel des règles :\n";
-    affiche_tableau ();
+    affiche_tableau coups;
     let ordi, joueur = ref 0, ref 0 in
     let rec boucle () =
       let mien = List.nth coups (Random.int (List.length coups)) in
@@ -35,7 +31,12 @@ module Make (Regles : R) : S = struct
       | "" -> () (* on ne se rappelle pas *)
       | cmd ->
         begin try
-          let sien = of_string (String.lowercase_ascii cmd) in
+          let sien =
+            if coups == Coup.coups then
+              of_string (String.lowercase_ascii cmd)
+            else
+              Std (Standard.of_string (String.lowercase_ascii cmd))
+          in
           Printf.printf "J'avais choisi %s.\n" (to_string mien);
           if mien > sien then (
             Printf.printf "Un point pour moi !\n";
@@ -59,18 +60,3 @@ module Make (Regles : R) : S = struct
       Printf.printf "Vous avez gagné...\n"
     else
       Printf.printf "Égalité.\n"
-end
-
-let () =
-  let m =
-    Printf.printf "Voulez-vous jouer avec le puits ? %!";
-    match read_line () with
-    | "oui" | "yes" ->
-      (module Make (Coup) : S)
-    | x ->
-      if x <> "non" && x <> "no" then
-        Printf.printf "On va dire que non...\n";
-      (module Make (Standard) : S)
-  in
-  let module M = (val m : S) in
-  M.main ()

3240c23dda9c0f4f9016944beaa5a12134b1ad2f
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-12-03 13:38:52 +0100

    Jeu interactif

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
     //static car appelée sans instance (ici par main)
     static void afficheTableau(Coup[] coups) {
@@ -15,11 +18,63 @@ public class App {
             System.out.println();
         }
     }
+
+    static String capitalize(String s) {
+        if (s.length() == 0)
+            return s;
+        else
+            return Character.toUpperCase(s.charAt(0)) +
+                s.substring(1).toLowerCase();
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println(Standard.Pierre.gagneContre(Standard.Ciseaux));
-        System.out.println(Coup.valueOf("Pierre"));
-        System.out.println(Puits.INSTANCE.gagneContre(Standard.Ciseaux));
-        afficheTableau(Standard.values());
-        afficheTableau(Coup.values());
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Voulez-vous jouer avec le puits ?");
+        String rep = sc.nextLine().toLowerCase();
+        Coup[] poss;
+        boolean puits;
+        //Class<? extends Coup> cl = Standard.class; //TODO essayer
+        if (rep.equals("oui") || rep.equals("yes")) {
+            poss = Coup.values();
+            puits = true;
+        }
+        else {
+            if (!rep.equals("non") && !rep.equals("no"))
+                System.out.println("On va dire que non...");
+            poss = Standard.values();
+            puits = false;
+        }
+        System.out.println("Petit rappel des règles :");
+        afficheTableau(poss);
+        Random ran = new Random();
+        int ordi = 0, joueur = 0;
+        while (true) {
+            Coup mien = poss[ran.nextInt(poss.length)]; //pas de triche !
+            System.out.println("Que jouez-vous ?");
+            String cmd = sc.nextLine();
+            if (cmd.length() == 0)
+                break;
+            try {
+                cmd = capitalize(cmd);
+                Coup sien = puits ? Coup.valueOf(cmd) : Standard.valueOf(cmd);
+                System.out.println(mien);
+                if (mien.gagneContre(sien)) {
+                    System.out.println("J'ai gagné !");
+                    ordi++;
+                }
+                else if (sien.gagneContre(mien)) {
+                    System.out.println("Vous avez gagné...");
+                    joueur++;
+                }
+                else
+                    System.out.println("Égalité");
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println("Pas compris...");
+            }
+        }
+        System.out.println("Votre score : " + joueur);
+        System.out.println("Mon score : " + ordi);
+        sc.close();
     }
 }
